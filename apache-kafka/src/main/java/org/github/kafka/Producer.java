31927d3d23542ba38a09c19c9893966b809bbb52
haxul <s.s.starodubov@yandex.ru>
2020-05-22 12:55:36 +0400

    create producer

---
+++ apache-kafka/src/main/java/org/github/kafka/Producer.java
@@ -0,0 +1,28 @@
+package org.github.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        //create producer props
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic","hi");
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        //send data
+
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
